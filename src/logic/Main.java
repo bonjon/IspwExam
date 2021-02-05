49043fce70584d601708d36cc6e5fdaa7c75794f
bonjon <45831678+bonjon@users.noreply.github.com>
2021-02-05 10:27:00 +0100

    Security Hotspot

--- src/logic/Main.java
+++ src/logic/Main.java
@@ -5,8 +5,12 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main extends Application {
+	
+	private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -18,7 +22,7 @@ public class Main extends Application {
 			primaryStage.setMaxHeight(370);
 			primaryStage.show();
 		} catch(Exception e) {
-			e.printStackTrace();
+			LOGGER.log(Level.WARNING, e.toString());
 		}
 	}
 	
